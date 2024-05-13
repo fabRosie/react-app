c60769403601e52659bd598be0758be2e7abd9bd
罗茜 <rosieluo@luoqiandeMacBook-Pro.local>
2024-05-13 16:37:07 +0800

    router

--- src/04-router/router/indexRouter.js
+++ src/04-router/router/indexRouter.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react'
-
-import { HashRouter,Route } from 'react-router-dom'
+import { BrowserRouter, HashRouter,Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom'
 
 // 引入组件
 import Films from '../views/Films'
 import Cinemas from '../views/Cinemas'
 import Center from '../views/Center'
 
+const routes = [
+  {
+    // it renders this element
+    element: <Films />,
+
+    // when the URL matches this segment
+    path: "/",
+
+    // with this data loaded before rendering
+    loader: async ({ request, params }) => {
+      return fetch(
+        `/fake/api/teams/${params.teamId}.json`,
+        { signal: request.signal }
+      );
+    },
+
+    // performing this mutation when data is submitted to it
+    action: async ({ request }) => {
+      // return updateFakeTeam(await request.formData());
+    },
+
+    // and renders this element in case something went wrong
+    // errorElement: <ErrorBoundary />,
+  },
+  {
+    element: <Films />,
+    path: "/films",
+  },
+  {
+    element: <Cinemas />,
+    path: "/cinemas",
+  },
+  {
+    element: <Center />,
+    path: "/center",
+  },
+]
+
+const router = createBrowserRouter(routes, {
+  basename: '/app'
+})
+
 export default class indexRouter extends Component {
   render() {
     return (
-      <div>
-        <HashRouter>
-          <Route path="/films" component = {Films}></Route>
-          <Route path="/cinemas" component = {Cinemas}></Route>
-          <Route path="/center " component = {Center}></Route>
-        </HashRouter>
-      </div>
+        <RouterProvider basename="/app" router={router} />
     )
   }
 }
