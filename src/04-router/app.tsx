38835dc13bc194cee9710a40096c22971f3c6755
Rosie <544268513@qq.com>
2024-05-17 22:03:49 +0800

    ts

--- src/04-router/app.tsx
+++ src/04-router/app.tsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
-import MRouter from './router/indexRouter'
+import Routers from './router/indexRouter'
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <MRouter></MRouter>
+        <Routers></Routers>
       </div>
     )
   }
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './04-router/app'
+
+ReactDOM.render(
+  <App></App>,
+  document.getElementById('root')
+)
